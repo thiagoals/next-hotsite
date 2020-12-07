8bb35bdea6181e632afeb21e7d1482eaa416c6ca
Thiago Augusto <thiago.lopes@ceuma.br>
2020-12-07 16:56:55 -0300

    Updating content for real business

--- lib/mirage/landingPageMock.tsx
+++ lib/mirage/landingPageMock.tsx
@@ -1,39 +1,39 @@
 import { createServer } from "miragejs";
 export const landingPage = {
   landingPage:{
-    Title: 'TH Solutions - The best of technology',
+    Title: 'TH Soluções - O melhor que a tecnologia tem a oferecer',
     Logo: { url: 'https://s1.logaster.com/static/v3/img/products/logo.png', name: 'logo.png' },
     Banner: { url: 'https://www.incimages.com/uploaded_files/image/1920x1080/getty_660582997_412145.jpg', name: 'banner.jpg' },
     Menus: [
-      { Description: 'About us', Link: '#aboutUs', Order: 1 },
-      { Description: 'Contact', Link: '#contact', Order: 2 }
+      { Description: 'Sobre nós', Link: '#aboutUs', Order: 1 },
+      { Description: 'Contato', Link: '#contact', Order: 2 }
     ],
-    BannerTitle: 'TH Solutions',
-    BannerDescription: 'Web developing you have never see.',
-    AboutUsTitle: 'About me',
-    AboutUsSubtitle: "My name is Thiago. I'm a fullstack developer. This website uses what is the best of web develop technology: NextJS.",
+    BannerTitle: 'TH Soluções',
+    BannerDescription: 'Desenvolvimento de sistemas como você nunca viu.',
+    AboutUsTitle: 'Um pouco sobre nós',
+    AboutUsSubtitle: "A TH soluções é uma empresa voltada para desenvolvimento de todo e qualquer software sob demanda.",
     AboutUsCards:[
       {
         Icon: 'CodeSlash',
-        Title: 'Coding',
-        Description: 'We have the best that technology can offer to you.',
-        HasButton: true,
+        Title: 'Codificação',
+        Description: 'Nós temos o que a tecnologia tem de melhor a oferecer.',
+        HasButton: false,
         ButtonName: 'See more.',
         ButtonHref: '#seeMore',
       },
       {
         Icon: 'Globe',
-        Title: 'Availability',
-        Description: 'We deploy in cloud, you can access anywhere you want.',
-        HasButton: true,
+        Title: 'Disponibilidade',
+        Description: 'Como trabalhamos em nuvem, você pode acessar onde estiver.',
+        HasButton: false,
         ButtonName: 'See more.',
         ButtonHref: '#seeMore',
       },
       {
         Icon: 'GraphUp',
-        Title: 'Business',
-        Description: 'You can boost your company by simply making a website.',
-        HasButton: true,
+        Title: 'Negócio',
+        Description: 'Alavanque o seu negócio com as nossas soluções sob demanda.',
+        HasButton: false,
         ButtonName: 'See more.',
         ButtonHref: '#seeMore',
       },
